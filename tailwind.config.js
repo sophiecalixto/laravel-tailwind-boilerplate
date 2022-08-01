0d1a15f7c7cfe89959fb59511c8ebbf29abfb9a5
Sophie Calixto <96028010+sophiecalixto@users.noreply.github.com>
2022-08-01 09:05:12 -0300

    tailwind config // set container and screens added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        container: {
+            center: true,
+            padding: "1.5rem",
+        },
+        screens: {
+            tablet: "640px",
+            // => @media (min-width: 640px) { ... }
+
+            laptop: "1024px",
+            // => @media (min-width: 1024px) { ... }
+
+            desktop: "1280px",
+            // => @media (min-width: 1280px) { ... }
+        },
+    },
+    plugins: [],
+};
